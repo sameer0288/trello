d042334ee493570c2736f1ac32cdfc7e2676476c
Sameer Ali <sameersheikh0288@gmail.com>
2026-04-22 09:27:52 +0530

    feat: initialize Prisma client instance with error and warning logging

--- server/src/utils/prisma.js
+++ server/src/utils/prisma.js
@@ -1,19 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
-const { PrismaPg } = require('@prisma/adapter-pg');
-const { Pool } = require('pg');
 
-const pool = new Pool({ 
-  connectionString: process.env.DATABASE_URL,
-  max: 20,
-  idleTimeoutMillis: 30000,
-  connectionTimeoutMillis: 2000,
+const prisma = new PrismaClient({
+  log: ['error', 'warn'],
 });
-
-pool.on('error', (err) => {
-  // Silent in production, only critical errors
-});
-
-const adapter = new PrismaPg(pool);
-const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
