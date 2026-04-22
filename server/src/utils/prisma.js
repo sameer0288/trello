0cc1bd5248914819cbac105e75ab8d64bd7c3518
Sameer Ali <sameersheikh0288@gmail.com>
2026-04-22 09:31:56 +0530

    feat: initialize Prisma client with PostgreSQL adapter and connection pooling

--- server/src/utils/prisma.js
+++ server/src/utils/prisma.js
@@ -1,7 +1,15 @@
 const { PrismaClient } = require('@prisma/client');
+const { PrismaPg } = require('@prisma/adapter-pg');
+const { Pool } = require('pg');
 
-const prisma = new PrismaClient({
-  log: ['error', 'warn'],
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  max: 10,
+  idleTimeoutMillis: 30000,
+  connectionTimeoutMillis: 5000, // Increased to 5s
 });
+
+const adapter = new PrismaPg(pool);
+const prisma = new PrismaClient({ adapter });
 
 module.exports = prisma;
